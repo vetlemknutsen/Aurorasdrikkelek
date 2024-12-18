3fab28d107e8b8aa67e16020a291d21de0abe41c
Vetle Knutsen <vetleknutsen1@gmail.com>
2024-12-18 18:22:44 +0100

    oppdatert frontend

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,17 @@
-export default function Page() {
-  return (
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="text-3xl font-bold">Velkommen til Drikkelek!</h1>
-      </div>
-  );
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        fetch('https://aurorasdrikkelek-production.up.railway.app/api/hello')
+            .then((res) => res.json())
+            .then((data) => setMessage(data.message));
+    }, []);
+
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="text-3xl font-bold">{message || 'Loading...'}</h1>
+        </div>
+    );
 }
