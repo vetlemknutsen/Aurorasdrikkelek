d21457488e371fbbbf0649f156348f452a860259
Vetle Knutsen <vetleknutsen1@gmail.com>
2024-12-18 17:36:53 +0100

    initiert prosjekt

---
+++ frontend/src/app/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-3xl font-bold">Velkommen til Drikkelek!</h1>
+      </div>
+  );
+}
