59f615a9bfc9626408d51f3095a99a62e51be5bd
Vetle Knutsen <vetleknutsen1@gmail.com>
2024-12-19 20:02:03 +0100

    test push

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const router = require("./index");
+const router = require("./index"); // import index.js
 
 const app = express();
 
-app.use(cors({
-    origin: '*',
-}));
-app.options('*', cors());
+app.use(cors());
 
 const dbUrl = process.env.MONGO_PUBLIC_URL;
 
@@ -19,3 +16,8 @@ mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(express.json());
 app.use(router);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
